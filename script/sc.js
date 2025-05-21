186cfeaa5d835dc8ec8160bccaab28c317694aad
grace <kellygrs@gmail.com>
2025-05-21 21:21:27 +0200

    guest

--- script/sc.js
+++ script/sc.js
@@ -1,21 +1,33 @@
-const list =["Roberto","Luis","Pedro","Juan","María","Laura","Carolina","Julia"];
+const list = ["Roberto", "Luis", "Pedro", "Juan", "María", "Laura", "Carolina", "Julia"];
 console.log(list);
 
-list.push("Juani","Alex");
+list.push("Juani", "Alex");
 console.log(list);
 
-list.splice(2,0,"Marta")
+list.splice(2, 0, "Marta")
 console.log(list);
 
 list.shift();
 console.log(list);
 
-const otherList= ["José","Vicente","Trina","Claudia","Beatríz"];
+const otherList = ["José", "Vicente", "Trina", "Claudia", "Beatríz"];
 console.log(otherList);
 
-const party= (list).concat(otherList);
+let party = (list).concat(otherList);
 console.log(party);
 
 
+let mayusParty = party.join(",");
+party=mayusParty.toUpperCase()
+console.log(party);
 
-// console.log(party.toUpperCase);
+let partyArray=party.split(",");
+console.log(partyArray);
+
+guest=prompt("¿Quién falta en la lista");
+let findGuests= partyArray.filter((missing)=>{
+    if (missing===guest) return true;
+    else return false;
+})
+console.log(findGuests);
+
